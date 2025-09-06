51fc57a32dc56c5c628f560d04e1407282023a80
AleemK75 <ak2514905@gmail.com>
2025-09-06 21:36:13 +0530

    project completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,7 +3,6 @@ import dotenv from "dotenv";
 import { connectDb } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import path from "path";
 
 import authRouter from "./routes/auth.route.js";
 import messageRouter from "./routes/message.route.js";
@@ -12,7 +11,7 @@ import { app, server } from "./lib/socket.js";
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
-const __dirname = path.resolve();
+
 
 // Body parsers
 app.use(express.json({ limit: "10mb" }));
@@ -22,44 +21,40 @@ app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use(cookieParser());
 
 // CORS for local development only
-if (process.env.NODE_ENV !== "production") {
-  const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
+
+
   app.use(
     cors({
-      origin: FRONTEND_URL,
+      origin: "http://localhost:5173",
       methods: ["GET", "POST", "PUT", "DELETE"],
       credentials: true,
     })
   );
-}
+
 
 // API routes
 
-
-app.get("/", (req, res) => {
-  res.send("API is running!");
-});
 app.use("/api/auth", authRouter);
 app.use("/api/messages", messageRouter);
 
-// Serve frontend in production
-if (process.env.NODE_ENV === "production") {
-  const frontendPath = path.join(__dirname, "../frontend/dist");
-  console.log("Serving frontend from:", frontendPath);
+// // Serve frontend in production
+// if (process.env.NODE_ENV === "production") {
+//   const frontendPath = path.join(__dirname, "../frontend/dist");
+//   console.log("Serving frontend from:", frontendPath);
 
-  app.use(express.static(frontendPath));
+//   app.use(express.static(frontendPath));
 
-  // Catch-all route for React router
-app.get("*", (req, res) => {
-  const indexFile = path.join(frontendPath, "index.html");
-  res.sendFile(indexFile, (err) => {
-    if (err) {
-      console.error("Error sending index.html:", err);
-      res.status(500).send("Server error");
-    }
-  });
-});
-}
+//   // Catch-all route for React router
+// app.get("*", (req, res) => {
+//   const indexFile = path.join(frontendPath, "index.html");
+//   res.sendFile(indexFile, (err) => {
+//     if (err) {
+//       console.error("Error sending index.html:", err);
+//       res.status(500).send("Server error");
+//     }
+//   });
+// });
+// }
 
 // Start server
 server.listen(PORT, () => {
