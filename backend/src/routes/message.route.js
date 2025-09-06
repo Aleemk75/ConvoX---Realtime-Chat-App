19b44fa2c987a704dd2a23a694089fbb8d4da7f6
AleemK75 <ak2514905@gmail.com>
2025-09-06 20:41:44 +0530

    chnage in the messages.route.js

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -4,7 +4,7 @@ import { protectRoute } from "../middlewares/auth.middleware.js";
 import { getUsers,getMessages,sendMessage } from "../controllers/message.controller.js";
 
 router.get("/users", protectRoute, getUsers);
-router.get("/:id",protectRoute,getMessages);
 router.post("/send/:id",protectRoute,sendMessage);
+router.get("/:id",protectRoute,getMessages);
 
 export default router;
