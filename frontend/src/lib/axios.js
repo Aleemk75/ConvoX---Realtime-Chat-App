727e1fddc9975a23c8af9bbad85c2cac6cfb510d
AleemK75 <ak2514905@gmail.com>
2025-09-07 21:12:06 +0530

    bug fixed

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:5001/api" || "https://convox-realtime-chat-app-backend.onrender.com/api",
+  baseURL: "http://localhost:5001/api" || "https://convox-realtime-chat-app-backend.onrender.com",
   withCredentials: true,
 });
