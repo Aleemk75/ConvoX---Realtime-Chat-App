1a6b3cdd3fbc405188a60753272f28c04e052aec
Aleem khan <ak2514905@gmail.com>
2025-09-23 01:19:10 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -23,11 +23,11 @@ app.use(cookieParser());
 // CORS for local development only
 
 
-const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
+// const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
 
 app.use(
   cors({
-    origin: FRONTEND_URL,
+    origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
