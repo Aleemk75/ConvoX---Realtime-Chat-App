96a28d9f80c910c148c26b3c122c0c638f4dd5fd
AleemK75 <ak2514905@gmail.com>
2025-09-26 00:20:27 +0530

    changes

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -3,7 +3,7 @@ import { axiosInstance } from "../lib/axios.js";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
-const BASE_URL = "http://localhost:5001" || "https://convox-realtime-chat-app-backend.onrender.com";
+const BASE_URL = "https://convox-realtime-chat-app-backend.onrender.com";
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
